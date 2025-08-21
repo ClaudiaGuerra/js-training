17ded044ec4eca58532112a5e3c90e05790ccca6
Claudia Guerra <claudiagbak@gmail.com>
2025-08-21 15:11:58 +0100

    Created node project, installed prompt library,created JS files for comments, print and input. Setup VSC to run JS code from IDE

---
+++ print.js
@@ -0,0 +1,6 @@
+// console.log -> Imprimir mensajes en la consola
+// Uso: console.log(mensaje) -> donde mensaje puede ser una cadena de texto, un número, un objeto, etc.
+console.log('Hola, mundo!');
+console.log(42);
+console.log({ clave: 'valor' });
+console.log(true);
