fa4ac25ff174ac1da182f6244ae606baffb9ac03
Claudia Guerra <claudiagbak@gmail.com>
2025-08-22 18:10:48 +0100

    Created 2.grade_average.js, this file contains homework to calculate the average of three grades provided by the user

---
+++ exercises/2.grade_average.js
@@ -0,0 +1,24 @@
+/*
+Objetivo: Calcular el promedio de 3 calificaciones
+Entrada: 
+    - Calificaciones (número)
+Salida:
+    - Promedio (número)
+*/
+
+// Necesario para solicitar datos al usuario
+const prompt = require('prompt-sync')();
+
+// 1. Solicitar 3 calificaciones al usuario. (Recuerda convertirlas a números)
+const cal1 = Number(prompt('Por favor, ingresa la primera calificación: '));
+const cal2 = Number(prompt('Por favor, ingresa la segunda calificación: '));
+const cal3 = Number(prompt('Por favor, ingresa la tercera calificación: '));
+
+// Comprueba el tipo de dato con typeof
+console.log('Las calificaciones son del tipo:', 'Calificación 1:', typeof cal1, ', Calificación 2:', typeof cal2, ', Calificación 3:', typeof cal3, );
+
+// 2. Calcula el promedio 
+const promedio = (cal1 + cal2 + cal3)/3;
+
+// 3. Imprime el promedio 
+console.log('El promedio es: ', promedio);
